5065086b1f81efaef1eb97bacd4bcd5245987420
georgenjobvu <george.njobvu01@gmail.com>
2023-02-18 11:20:45 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints numbers
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		printf("%d \n", a);
+		a++;
+	}
+	return (0);
+}
